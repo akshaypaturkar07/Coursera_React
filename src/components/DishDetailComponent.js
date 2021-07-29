ff50dfa431452421a4dbaeb4d50af7e84dfd92fb
Akky <akshaydp07@gmail.com>
2021-07-29 12:47:25 +0530

    Before assignment 1

---
+++ src/components/DishDetailComponent.js
@@ -0,0 +1,35 @@
+import { Component } from "react";
+
+class DishDetail extends Component{
+    render() {
+        const menu = this.props.dishes.map((dish) => {
+            return (
+              <div  className="col-12 col-md-5 m-1">
+                <Card key={dish.id}
+                  onClick={() => this.onDishSelect(dish)}>
+                  <CardImg width="100%" src={dish.image} alt={dish.name} />
+                  <CardImgOverlay>
+                      <CardTitle>{dish.name}</CardTitle>
+                  </CardImgOverlay>
+                </Card>
+              </div>
+            );
+        });
+
+        return (
+            <div className="container">
+                <div className="row">
+                    {menu}
+                </div>
+                <div className="row">
+                  <div  className="col-12 col-md-5 m-1">
+                    {this.renderDish(this.state.selectedDish)}
+                  </div>
+                </div>
+            </div>
+        );
+    }
+
+
+}
+export default DishDetail;
